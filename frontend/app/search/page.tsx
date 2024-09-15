53fed22277824b361691d16bf5986185dac955b0
FagerholmEmil <blazyvici@gmail.com>
2024-09-15 16:57:15 -0700

    suspense

--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSearchParams } from 'next/navigation';
 import DronePreferencesForm from '@/components/DronePreferencesForm';
+import { Suspense } from 'react';
 
 export default function SearchPage() {
   const searchParams = useSearchParams();
@@ -16,9 +17,10 @@ export default function SearchPage() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1>Search Results for: {query}</h1>
-     
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1>Search Results for: {query}</h1>
+      </div>
+    </Suspense>
   );
 }
